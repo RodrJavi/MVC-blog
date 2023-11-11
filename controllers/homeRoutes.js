32e786ceaf9b7bb677ed8acf72799a77af16eba9
Javi <nb2javi@gmail.com>
2023-11-11 15:06:05 -0600

    Added routs to homeroutes and added handlebar files for all applicable pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,4 +10,29 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+router.get("/dashboard", (req, res) => {
+  try {
+    res.render("dashboard");
+  } catch {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
