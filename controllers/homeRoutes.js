6625d3c412fd8c52ac22372d84d6c23c38c8dbf8
Javi <nb2javi@gmail.com>
2023-11-11 16:57:16 -0600

    Logging in and out functional

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -43,9 +43,11 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", withAuth, (req, res) => {
   try {
-    res.render("dashboard");
+    res.render("dashboard", {
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
